3b13a5f60b689aa63a6ebecf1060f6d5f15b9ecb
Ayan Sengupta <ayan.sengupta27@gmail.com>
2018-11-08 04:51:39 -0500

    first commit

---
+++ build/precache-manifest.694874d17751692ff19849b9d42959c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fba7f2b2bf72dbe84d0c",
+    "url": "/ContactManager/static/js/runtime~main.fba7f2b2.js"
+  },
+  {
+    "revision": "430878450c7bc5e49ce2",
+    "url": "/ContactManager/static/js/main.43087845.chunk.js"
+  },
+  {
+    "revision": "634265a8f07d1cc9164f",
+    "url": "/ContactManager/static/js/1.634265a8.chunk.js"
+  },
+  {
+    "revision": "430878450c7bc5e49ce2",
+    "url": "/ContactManager/static/css/main.f5741332.chunk.css"
+  },
+  {
+    "revision": "634265a8f07d1cc9164f",
+    "url": "/ContactManager/static/css/1.5aea661b.chunk.css"
+  },
+  {
+    "revision": "9967f81748fcb0e8db659f5686c74e47",
+    "url": "/ContactManager/index.html"
+  }
+];
